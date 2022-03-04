886464db93afa67517873c234ede5cba09b382b8
naveenkodiganti57@gmail.com <naveenkodiganti57@gmail.com>
2022-03-04 09:11:32 -0500

    first commit

---
+++ ProjectK.Games.LCR/ProjectK.Games.LCR.Views/GameSettingsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ProjectK.Games.LCR.Views
+{
+    public partial class GameSettingsView : UserControl
+    {
+        public GameSettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProjectK.Games.LCR/ProjectK.Games.LCR.WinApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ProjectK.Games.LCR.WinApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
